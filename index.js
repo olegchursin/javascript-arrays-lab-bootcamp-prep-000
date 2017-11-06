eee27e6a780a2ecd0061347bbd28474d5dedd1a9
Oleg Chursin <olegchursin@gmail.com>
2017-11-06 20:38:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens.push();
+  kittens.push(name);
   return kittens;
 }
-
-destructivelyAppendKitten('Ralph');
